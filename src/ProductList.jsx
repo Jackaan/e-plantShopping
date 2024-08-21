a26a23db66082803d657667ada3561a0640f44b4
jackaan <jacknoaksson@hotmail.com>
2024-08-21 10:49:39 -0400

    Added functionality to add plants to cart

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { addItem } from './CartSlice';
+
 function ProductList() {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-    
+    const [addedToCart, setAddedToCart] = useState({});
 
     const plantsArray = [
         {
@@ -241,6 +243,14 @@ function ProductList() {
         e.preventDefault();
         setShowPlants(true); // Set showAboutUs to true when "About Us" link is clicked
         setShowCart(false); // Hide the cart when navigating to About Us
+    };
+
+    const handleAddToCart = (product) => {
+        dispatch(addItem(product));
+        setAddedToCart((prevState) => ({
+            ...prevState,
+            [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
+        }));
     };
 
     const handleContinueShopping = (e) => {
